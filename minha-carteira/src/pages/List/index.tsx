e97dcdbe7dd09929465a7e47a7f2ac833a5b4a69
brunoemferreira <brunoemf@gmail.com>
2021-04-13 23:48:34 -0300

    Creating Filters

--- minha-carteira/src/pages/List/index.tsx
+++ minha-carteira/src/pages/List/index.tsx
@@ -3,20 +3,50 @@ import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import SelectInput from '../../components/SelectInput';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
-  const frutas = [
-    { value: 'Banana', label: 'Banana' },
-    { value: 'Abacaxi', label: 'Abacaxi' },
-    { value: 'Uva', label: 'Uva' }
+  const months = [
+    { value: 1, label: 'Janeiro' },
+    { value: 2, label: 'Fevereiro' },
+    { value: 3, label: 'Março' },
+    { value: 4, label: 'Abril' },
+    { value: 5, label: 'Maio' },
+    { value: 6, label: 'Junho' },
+    { value: 7, label: 'Julho' },
+    { value: 8, label: 'Agosto' },
+    { value: 9, label: 'Setembro' },
+    { value: 10, label: 'Outubro' },
+    { value: 11, label: 'Novembro' },
+    { value: 12, label: 'Dezembro' }
+  ];
+
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2021, label: 2021 }
+
   ];
 
   return (
     <Container>
       <ContentHeader title="Lista" lineColor="red" >
-        <SelectInput options={frutas} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+      <Filters>
+        <button
+          type="button"
+          className="tag-filter tag-filter-recurrent"
+        >
+          Recorrentes
+          </button>
+        <button
+          type="button"
+          className="tag-filter tag-filter-eventual"
+        >
+          Eventuais
+          </button>
+      </Filters>
       <Content>
         <HistoryFinanceCard
           tagColor="#E44c4E"
